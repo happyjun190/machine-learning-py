ee8e9d357cdd0d13a6b35c677edd6b249f42815a
吴申俊 <wsj@wsj.com>
2018-04-17 16:50:18 +0800

    熵计算-朴素贝叶斯分类

---
+++ chapter4/chapter_4_1.py
@@ -0,0 +1,46 @@
+from chapter4 import bayes
+import re
+
+#针对标点符号进行正则匹配
+#regEx = re.compile('\\W*')
+#mySent = "This book is the best book on Python or M.L. I have ever laid eyes upon."
+
+#listOfTokens = regEx.split(mySent)
+#print(listOfTokens)
+
+#listOfTokens = [word.lower() for word in listOfTokens if len(word) > 0]
+#print(listOfTokens)
+
+#emailText = open('email/ham/6.txt').read()
+#listOfTokens = regEx.split(emailText)
+#listOfTokens = [word.lower() for word in listOfTokens if len(word) > 0]
+#print(listOfTokens)
+
+bayes.spamTest()
+
+exit()
+
+
+bayes.testingNB()
+
+exit()
+listOPosts, listClasses = bayes.loadDataSet()
+
+myVocabList = bayes.createVocableList(listOPosts)
+print(myVocabList)
+
+
+trainMat = []
+
+for postinDoc in listOPosts:
+    #print(postinDoc)
+    trainMat.append(bayes.setOfWords2Vec(myVocabList, postinDoc))
+
+
+p0V, p1V, pAb = bayes.trainNB0(trainMat, listClasses)
+
+print(pAb)
+print(p0V)
+print(p1V)
+
+#print(trainMat)
