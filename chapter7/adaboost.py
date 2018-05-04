869076e8b87b37748d7bfab4fe0b9bc213dc2cea
吴申俊 <wsj@wsj.com>
2018-05-04 17:10:19 +0800

    adaboost algorithm

--- chapter7/adaboost.py
+++ chapter7/adaboost.py
@@ -14,9 +14,12 @@ def loadSimpData():
 def stumpClassify(dataMatrix, dimen, threshVal, threshIneq):
     retArray = ones((shape(dataMatrix)[0], 1))
     if threshIneq == 'lt':
+        #print('threshIneq: %s, dataMatrix[:, dimen]: %s. threshVal: %s' % (threshIneq, dataMatrix[:, dimen], threshVal))
         retArray[dataMatrix[:, dimen] <= threshVal] = -1.0
     else:
+        #print('threshIneq: %s, dataMatrix[:, dimen]: %s. threshVal: %s' % (threshIneq, dataMatrix[:, dimen], threshVal))
         retArray[dataMatrix[:, dimen] > threshVal] = -1.0
+    #print("retArray: %s" % retArray)
     return retArray
 
 def buildStump(dataArr, classLabels, D):
@@ -31,12 +34,14 @@ def buildStump(dataArr, classLabels, D):
         for j in range(-1, int(numSteps) + 1):
             for inequal in ['lt', 'gt']:
                 threshVal = (rangeMin + float(j) * stepSize)
+                #print(threshVal)
                 predictedVals = stumpClassify(dataMatrix, i, threshVal, inequal)
                 errArr = mat(ones((m, 1)))
 
                 #print(predictedVals == labelMat)
 
                 errArr[predictedVals == labelMat] = 0
+                #print(errArr)
                 weightedError = D.T * errArr
 
                 #print("split: dim %d, thresh %.2f, thresh ineqal:  %s, the weighted error is %.3f" % (i, threshVal, inequal, weightedError))
@@ -48,5 +53,39 @@ def buildStump(dataArr, classLabels, D):
                     bestStump['thresh'] = threshVal
                     bestStump['ineq'] = inequal
     return bestStump, minError, bestClasEst
+
+#Listing 7.2 AdaBoost training with decision stumps
+def adaBoostTrainDS(dataArr, classLabels, numIt=40):
+    weakClassArr = []
+    m = shape(dataArr)[0]
+    D = mat(ones((m, 1)) / m)
+    aggClassEst = mat(zeros((m, 1)))
+    for i in range(numIt):
+        bestStump, error, classEst = buildStump(dataArr, classLabels, D)
+        print('D: %s' % D.T)
+        alpha = float(0.5 * log((1 - error)/ max(error, 1e-16)))
+        bestStump['alpha'] = alpha
+        weakClassArr.append(bestStump)
+        print('classEst: %s' % classEst.T)
+        expon = multiply(-1 * alpha * mat(classLabels).T, classEst)
+        D = multiply(D, exp(expon))
+        D = D / D.sum()
+        aggClassEst += alpha * classEst
+        print('aggClassEst: %s' % classEst.T)
+        aggErrors = multiply(sign(aggClassEst) != mat(classLabels).T, ones((m, 1)))
+        errorRate = aggErrors.sum() / m
+        print('total error: %f' % errorRate)
+        if errorRate == 0.0: break
+    return weakClassArr
+
+#Listing 7.3 AdaBoost classification function
+def adaClassify(datToClass, classifierArr):
+    dataMatrix = mat(datToClass)
+    m = shape(dataMatrix)[0]
+    aggClassEst = mat(zeros((m, 1)))
+    for i in range(len(classifierArr)):
+        classEst = stumpClassify(dataMatrix, classifierArr[i]['dim'], classifierArr[i]['thresh'], classifierArr[i]['ineq'])
+        aggClassEst += classifierArr[i]['alpha'] * classEst
+    return sign(aggClassEst)
 
 
--- chapter7/chapter_7_1.py
+++ chapter7/chapter_7_1.py
@@ -3,6 +3,12 @@ from numpy import *
 
 dataMat, classLabels = adaboost.loadSimpData()
 
+classifierArray = adaboost.adaBoostTrainDS(dataMat, classLabels, 9)
+
+#print(classifierArray)
+
+exit()
+
 D = mat(ones((5,1))/5)
 
 bestStump, minError, bestClasEst = adaboost.buildStump(dataMat, classLabels, D)
